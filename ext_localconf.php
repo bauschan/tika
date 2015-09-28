c8c19c8f7b9ad34ef754d4e9b46efe1542bab51b
Peter Foerger <peter.foerger@dkd.de>
2015-09-28 16:52:22 +0200

    Removed version compare

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,8 +6,3 @@ if (!defined('TYPO3_MODE')) {
 $metaDataExtractorRegistry = \TYPO3\CMS\Core\Resource\Index\ExtractorRegistry::getInstance();
 $metaDataExtractorRegistry->registerExtractionService('ApacheSolrForTypo3\\Tika\\Service\\Extractor\\MetaDataExtractor');
 $metaDataExtractorRegistry->registerExtractionService('ApacheSolrForTypo3\\Tika\\Service\\Extractor\\LanguageDetector');
-
-if (version_compare(TYPO3_version, '7.1', '>')) {
-	$textExtractorRegistry = \TYPO3\CMS\Core\Resource\TextExtraction\TextExtractorRegistry::getInstance();
-	$textExtractorRegistry->registerTextExtractor(\ApacheSolrForTypo3\Tika\Service\Extractor\TextExtractor::class);
-}
